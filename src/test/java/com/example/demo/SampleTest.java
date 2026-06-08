1b774f64d7c07a313681efb9dca9d443d82564c3
Your Name <Your Email>
2026-06-08 10:59:45 +0900

    演習431

---
+++ src/test/java/com/example/demo/SampleTest.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SampleTest {
+
+    @Test
+    void shimadaTest() {
+
+        int result = 0;
+
+        assertEquals(0, result);
+    }
+
+}
